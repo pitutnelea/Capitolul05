5d303abce634eb7630ddd2fc5d1a971041e43d1d
Pitut Nelea <pitut.nelea@yahoo.com>
2019-11-11 23:06:00 +0200

    -

--- Tema5.2-Parametrii_bata-i_vina/main.cpp
+++ Tema5.2-Parametrii_bata-i_vina/main.cpp
@@ -7,14 +7,12 @@ void Ameteala(int parSiVal, int&parTyRef, int*parTotPont)
 parSiVal *=3;
 parTyRef *=3;
 *parTotPont *=2;
-cout<<"In functie"<<parSiVal<<" "<<parTyRef<<" "<<&parTotPont<<endl;
 }
 
 int main()
 {
 int druckenRat = 3;
 Ameteala(druckenRat, druckenRat, &druckenRat);
-cout<<"Dupa functie"<<druckenRat<<" "<<druckenRat<<" "<<&druckenRat<<endl;
 cout<<druckenRat<<endl;
 
 return 0;
